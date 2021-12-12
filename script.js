1c04f022112dc8be07ad942fefc8500cb9d9cabf
antonymburia <antonymburia10@gmail.com>
2021-12-12 03:24:02 +0300

    create javascript variable to hold user data

---
+++ script.js
@@ -0,0 +1,8 @@
+function namakangeneratore() {
+  // declare variable
+  var name = document.getElementById('name').value;
+  var year = parseInt(document.getElementById('yob').value);
+  var b_month = parseInt(document.getElementById('birth_month').value);
+  var day = parseInt(document.getElementById('dob').value);
+  var gender = document.getElementById('gender').value;
+}
