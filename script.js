a9a1fbd93e03f17c5f98726df0485d1b3db1a6fc
antonymburia <antonymburia10@gmail.com>
2021-12-12 03:28:28 +0300

    get user year and exact month

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ function namakangeneratore() {
   var b_month = parseInt(document.getElementById('birth_month').value);
   var day = parseInt(document.getElementById('dob').value);
   var gender = document.getElementById('gender').value;
+
+  // get exact date and month
+  var current_year = new Date().getFullYear();
+  var month = b_month -1;
 }
