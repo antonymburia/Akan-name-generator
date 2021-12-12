cb52c58183604ad553ddd4b8851652443ab96649
antonymburia <antonymburia10@gmail.com>
2021-12-12 03:42:52 +0300

    get the week day's number

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ function namakangeneratore() {
   // get exact date and month
   var current_year = new Date().getFullYear();
   var month = b_month -1;
+  // get day number
+  let date, week_day;
+  date = new Date(year, month, day);
+  week_day = date.getDay();
+  // validate year month and day
+  if (year < 1000 || year > current_year) {
+    alert('enter a valid year');
+  } else {
+    if (month < 1 || month > 31) {
+      alert('enter a valid month');
+    }
+  }
 
   // store names and days arrays
   var male_names = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
