f47218b8f470eec1f22c4d5c8bfc5cf1d957a95a
antonymburia <antonymburia10@gmail.com>
2021-12-12 04:21:00 +0300

    check input data to get respective name

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function namakangeneratore() {
+function akangenerator() {
   // declare variable
-  var name = document.getElementById('name').value;
+  var fname = document.getElementById('name').value;
   var year = parseInt(document.getElementById('yob').value);
   var b_month = parseInt(document.getElementById('birth_month').value);
   var day = parseInt(document.getElementById('dob').value);
@@ -9,10 +9,6 @@ function namakangeneratore() {
   // get exact date and month
   var current_year = new Date().getFullYear();
   var month = b_month -1;
-  // get day number
-  let date, week_day;
-  date = new Date(year, month, day);
-  week_day = date.getDay();
   // validate year month and day
   if (year < 1000 || year > current_year) {
     alert('enter a valid year');
@@ -24,5 +20,18 @@ function namakangeneratore() {
 
   // store names and days arrays
   var male_names = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-  var female_names = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];}
+  var female_names = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
   var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  // get day number
+  let date, week_day;
+  date = new Date(year, month, day);
+  week_day = date.getDay();
+  // check the input data and give respective akan name
+  if (gender == "male") {
+    document.getElementById("message").innerHTML = ("Hello " + fname + " You were born on a " + days[week_day] + " Your Akan Name is " +  male_names[week_day]);
+  } else {
+    if (gender == "female") {
+      document.getElementById("message").innerHTML = ("Hello " + fname + " You were born on a " + days[week_day] + " Your Akan Name is " +  female_names[week_day]);
+    }
+  }
+}
