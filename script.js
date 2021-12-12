4bb21baf08626f3e1bb7a5a93f38169292a90394
antonymburia <antonymburia10@gmail.com>
2021-12-12 19:03:22 +0300

    create js form validation to check user data

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 function akangenerator() {
+    // form validation
+  if( document.getElementById('name').value == "" ) {
+       alert( "Please provide your name!" );
+       return false;
+    }
+  if( document.getElementById('yob').value == "" ) {
+       alert( "Please provide your year of birth!" );
+       return false;
+  }
+  if( document.getElementById('birth_month').value == "" ) {
+      alert( "Please provide your birth month!" );
+      return false;
+   }
+  if( document.getElementById('dob').value == "" ) {
+    alert( "Please provide your date of birth!" );
+    document.getElementById('dob').value.focus();
+    return false;
+   }
+ 
+  
   // declare variable
   var fname = document.getElementById('name').value;
   var year = parseInt(document.getElementById('yob').value);
   var b_month = parseInt(document.getElementById('birth_month').value);
   var day = parseInt(document.getElementById('dob').value);
   var gender = document.getElementById('gender').value;
-
+      
   // get exact date and month
   var current_year = new Date().getFullYear();
   var month = b_month -1;
@@ -31,6 +51,7 @@ function akangenerator() {
   let date, week_day;
   date = new Date(year, month, day);
   week_day = date.getDay();
+  
   // check the input data and give respective akan name
   if (gender == "male") {
     document.getElementById("message").innerHTML = ("Hello " + fname + " You were born on a " + days[week_day] + " Your Akan Name is " +  male_names[week_day]);
@@ -40,10 +61,8 @@ function akangenerator() {
     }
   }
 }
+ 
 // clear form function
-function newFunction() {
-  
-}
 function clearform() {
   document.getElementById("form").reset();
 }
